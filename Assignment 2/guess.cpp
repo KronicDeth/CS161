6d94bbe75214c7e158787933664a5169da40c8dd
Luke Imhoff <Kronic.Deth@gmail.com>
2014-07-06 11:39:27 -0500

    Move guess.cpp to match structure of master

--- Assignment 2/guess.cpp
+++ Assignment 2/guess.cpp
@@ -1,7 +1,3 @@
-// Regina Imhoff
-// Ecampus CS 161
-// Assignment #2 
-
 #include <iostream> // includes and statements section
 
 using std::cin; // user input
@@ -10,61 +6,57 @@ using std::endl; // for line breaks
 
 int main () {
 
-   int magicNumber; // number player 1 enters 
-   int guess; // number that player 2 guesses
-   bool correct = false; // for when the user has correctly guessed the number
-   int tries = 3; // you get 3 tries
-   
-   cout << "Let's play a number guessing game!" << endl;
-   cout << "Ready Player 1:" << endl;
-   cout << "Player 1:  please pick a number for Player 2 to guess." << endl;
-   cout << "This number needs to be between 0 and 50." << endl;
-   cout << "Pick your secret number now: " << endl;
-   cin >> magicNumber;
-   
-   int number = 50;
-   while (number >= 0) // makes blank lines for player 2
-   {
-      cout << endl;
-      number--;
-    }
-   
-   cout << "Ready Player 2!" << endl;
-   cout << "You're going to now try to guess Player 1's secret number." << endl;
-   cout << "This number will be between 0 and 50" << endl;
-   cout << "You have 3 tries!" << endl;
-   
-do {
-  cout << "What is your guess?" << endl;
-  cin >> guess;
-  
-  if (guess < magicNumber)
-  
-  {  	
-    cout << "You guessed too low!" << endl;
-  }   
-  else if (guess > magicNumber)
-  
-  {
-    cout << "You guessed too high!" << endl;
-  }
-  else if (guess == magicNumber)
-  {
-    cout << "Your guess is correct!  Huzzah!" << endl;
-   }
-}  while ((guess != magicNumber) && (tries >= 3));
-      
+	int magicNumber; // number player 1 enters 
+	int guess; // number that player 2 guesses
+	bool correct = false; // for when the user has correctly guessed the number
+	int tries = 3; // you get 3 tries
 
-   for(int x = 0; x < 3 && (guess != magicNumber); x++, tries--)
-   {
-      tries++;
-    } 
-   
-   
-   if(tries == 0)
-      cout << "Sorry, you lose.  :(" << endl;
-      cout << "The correct number was " << magicNumber << endl;
-      
-   
-   return 0;
+	cout << "Let's play a number guessing game!" << endl;
+	cout << "Ready Player 1:" << endl;
+	cout << "Player 1:  please pick a number for Player 2 to guess." << endl;
+	cout << "This number needs to be between 0 and 50." << endl;
+	cout << "Pick your secret number now: " << endl;
+	cin >> magicNumber;
+
+	int number = 50;
+	while (number >= 0)
+	{
+		cout << endl;
+		number--;
+	}
+
+	cout << "Ready Player 2!" << endl;
+	cout << "You're going to now try to guess Player 1's secret number." << endl;
+	cout << "This number will be between 0 and 50" << endl;
+	cout << "You have 3 tries!" << endl;
+
+	do {
+		cout << "What is your guess?" << endl;
+		cin >> guess;
+
+		if (guess < magicNumber)
+		{  	
+			cout << "You guessed too low!" << endl;
+		}   
+		else if (guess > magicNumber)
+		{
+			cout << "You guessed too high!" << endl;
+		}
+		tries--;
+	}  while ((guess != magicNumber) && (tries > 0));
+
+
+	/* for(int x = 0; x < 3 && (guess != magicNumber); x++, tries--)
+	 *    {
+	 *          tries++;
+	 *              } */
+
+        if (guess == magicNumber) {
+		cout << "Your guess is correct!  Huzzah!" << endl;
+        } else {
+		cout << "Sorry, you lose.  :(" << endl;
+		cout << "The correct number was " << magicNumber << endl;
+	}
+
+	return 0;
 }
